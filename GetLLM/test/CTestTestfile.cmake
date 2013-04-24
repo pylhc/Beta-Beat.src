ba88960755a87a1f154900f2c3ecddae1e7bd589
vimaier <Viktor.Maier@cern.ch>
2013-04-24 14:42:42 +0200

    Changed modified GetLLM version to GetLLM_dev.py
    Since we are still developing on _dev version I changed
    CTestTestfile.cmake so filecheck.py will check the _dev version instead
    of GetLLM.py.

--- GetLLM/test/CTestTestfile.cmake
+++ GetLLM/test/CTestTestfile.cmake
@@ -1,7 +1,7 @@
 add_test(filecheck_getllm python ${CTEST_SOURCE_DIRECTORY}/GetLLM/test/filecheck.py 
 				-o 0
 				-v ${CTEST_SOURCE_DIRECTORY}/GetLLM/test/GetLLM_valid.py
-				-m ${CTEST_SOURCE_DIRECTORY}/GetLLM/test/../GetLLM.py
+				-m ${CTEST_SOURCE_DIRECTORY}/GetLLM/test/../GetLLM_dev.py
 				-p ${CTEST_SOURCE_DIRECTORY}/GetLLM/test/data
 				-s ${CTEST_BINARY_DIRECTORY}
 				)
