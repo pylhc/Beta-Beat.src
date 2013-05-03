8dc5dfba42fa367c88c661d20dcc66336232b88d
Rogelio Tomas Garcia <rtomas@pcbe13741.cern.ch>
2013-05-03 10:19:04 +0200

    Fixed the link statements in Generateslinks.sh

--- GenerateLinks.sh
+++ GenerateLinks.sh
@@ -1,23 +1,20 @@
 
-
-#find ./ -type l -delete 
-
-ln -s ./Analyses/madxrunner.py ./Python_Classes4MAD/madxrunner.py
-ln -s ./beta_Phase/SAD/BetaAnalysis.sad /afs/cern.ch/user/a/amorita/public/mirror-ldata/LHC/SAD/BetaAnalysis.sad
-ln -s ./beta_Phase/JorgW/SPS/BetaAnalysis.sad /afs/cern.ch/user/a/amorita/public/mirror-ldata/LHC/SAD/BetaAnalysis.sad
-ln -s ./beta_Phase/JorgW/LHC/830 /nfs/cs-ccr-nfs4/lhc_data/OP_DATA/FILL_DATA/830
-ln -s ./beta_Phase/JorgW/LHC/831 /nfs/cs-ccr-nfs4/lhc_data/OP_DATA/FILL_DATA/831
-ln -s ./Correction/Iterative/TEST/b1/filedes ./Correction/Iterative/TEST/b1/
-ln -s ./Correction/Iterative/TEST/b1/fileloc /afs/cern.ch/eng/sl/lintrack/Beta-Beat.src/
-ln -s ./MODEL/LHCB1/model ./MODEL/LHCB/model/
-ln -s ./MODEL/LHCB1/SimulateData /afs/cern.ch/user/r/rtomas/w1/MAD/LHC/Beta-Beat/SimulateData/
-ln -s ./MODEL/LHCB1/fullresponse ./MODEL/LHCB/fullresponse/
-ln -s ./MODEL/LHCB2/model ./MODEL/LHCB/model/
-ln -s ./MODEL/LHCB2/fullresponse ./MODEL/LHCB/fullresponse/
-ln -s ./MODEL/RHIC/ds/rhic /afs/cern.ch/user/r/rcalaga/mad/rhic
-ln -s ./MODEL/LHCB/model/StrengthFiles/Nominal /afs/cern.ch/eng/lhc/optics/V6.503/
-ln -s ./MODEL/LHCB/model/StrengthFiles/ATS /afs/cern.ch/eng/lhc/optics/ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/
-ln -s ./Python_Classes4MAD ../Python_Classes4MAD/
-ln -s ./DRIVE_src.Massimo/ifort /afs/cern.ch/sw/IntelSoftware/linux/x86_64/Compiler/11.1/072/bin/intel64/ifort
-ln -s ./PSB_orbit_correction/DODO-Export /user/psbop/images/DODO-Export/
-ln -s ./PSB_orbit_correction/Q_meas /user/psbop/data/2012/Q_meas/
+ln -s ../Python_Classes4MAD/madxrunner.py ./Analyses/madxrunner.py
+ln -s /afs/cern.ch/user/a/amorita/public/mirror-ldata/LHC/SAD/BetaAnalysis.sad ./beta_Phase/SAD/BetaAnalysis.sad
+ln -s /afs/cern.ch/user/a/amorita/public/mirror-ldata/LHC/SAD/BetaAnalysis.sad ./beta_Phase/JorgW/SPS/BetaAnalysis.sad
+ln -s /nfs/cs-ccr-nfs4/lhc_data/OP_DATA/FILL_DATA/830 ./beta_Phase/JorgW/LHC/830
+ln -s /nfs/cs-ccr-nfs4/lhc_data/OP_DATA/FILL_DATA/831 ./beta_Phase/JorgW/LHC/831
+ln -s ./ ./Correction/Iterative/TEST/b1/filedes
+ln -s /afs/cern.ch/eng/sl/lintrack/Beta-Beat.src/ ./Correction/Iterative/TEST/b1/fileloc
+ln -s ../LHCB/model/ ./MODEL/LHCB1/model
+ln -s /afs/cern.ch/user/r/rtomas/w1/MAD/LHC/Beta-Beat/SimulateData/ ./MODEL/LHCB1/SimulateData
+ln -s ../LHCB/fullresponse/ ./MODEL/LHCB1/fullresponse
+ln -s ../LHCB/model/ ./MODEL/LHCB2/model
+ln -s ../LHCB/fullresponse/ ./MODEL/LHCB2/fullresponse
+ln -s /afs/cern.ch/user/r/rcalaga/mad/rhic ./MODEL/RHIC/ds/rhic
+ln -s /afs/cern.ch/eng/lhc/optics/V6.503/ ./MODEL/LHCB/model/StrengthFiles/Nominal
+ln -s /afs/cern.ch/eng/lhc/optics/ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/ ./MODEL/LHCB/model/StrengthFiles/ATS
+ln -s ../Python_Classes4MAD/ ./Python_Classes4MAD
+ln -s /afs/cern.ch/sw/IntelSoftware/linux/x86_64/Compiler/11.1/072/bin/intel64/ifort ./DRIVE_src.Massimo/ifort
+ln -s /user/psbop/images/DODO-Export/ ./PSB_orbit_correction/DODO-Export
+ln -s /user/psbop/data/2012/Q_meas/ ./PSB_orbit_correction/Q_meas
