c4540bd5e595c82d083730a9f698a90fd99e6b66
vimaier <Viktor.Maier@cern.ch>
2013-05-27 15:31:38 +0200

    Added example and template files
    Added also some __init__.py files. They indicate folder as packages in
    PyDev.

---
+++ Utilities/templates/template_class_module.py
@@ -0,0 +1,24 @@
+'''
+Created on 27 May 2013
+
+@author: vimaier
+
+@version: 0.0.1
+
+templates.template_class_module <description>
+
+Change history:
+ - <version>, <author>, <date>:
+    <description>
+'''
+
+class ExampleClass(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
