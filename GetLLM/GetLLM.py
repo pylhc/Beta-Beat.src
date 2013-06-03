9c91b961c70fbf8cee5230326c7213c606e3dc5f
vimaier <Viktor.Maier@cern.ch>
2013-06-03 18:11:21 +0200

    Added the path to Beta-Beat(afs) to GetLLM

--- GetLLM/GetLLM.py
+++ GetLLM/GetLLM.py
@@ -178,7 +178,10 @@ VERSION='V2.38b PRO'
 ####
 
 import sys
-sys.path.append('/afs/cern.ch/eng/sl/lintrack/Python_Classes4MAD/')
+if "/afs/cern.ch/eng/sl/lintrack/Python_Classes4MAD/" not in sys.path: # add internal path for python scripts to current environment (tbach)
+    sys.path.append('/afs/cern.ch/eng/sl/lintrack/Python_Classes4MAD/')
+if "/afs/cern.ch/eng/sl/lintrack/Beta-Beat.src/" not in sys.path: # add internal path for python scripts to current environment (tbach)
+    sys.path.append('/afs/cern.ch/eng/sl/lintrack/Beta-Beat.src/')
 import traceback
 import metaclass
 import os
