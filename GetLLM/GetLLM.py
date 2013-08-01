1e5664a634b925a881508e7b70ce47ddfd646e00
vimaier <Viktor.Maier@cern.ch>
2013-08-01 18:08:55 +0200

    Deleted absolute path to bb dir to see effect

--- GetLLM/GetLLM.py
+++ GetLLM/GetLLM.py
@@ -190,8 +190,7 @@ V2.38b 03/dec/2012, tbach:
 import sys
 if "/afs/cern.ch/eng/sl/lintrack/Python_Classes4MAD/" not in sys.path: # add internal path for python scripts to current environment (tbach)
     sys.path.append('/afs/cern.ch/eng/sl/lintrack/Python_Classes4MAD/')
-if "/afs/cern.ch/eng/sl/lintrack/Beta-Beat.src/" not in sys.path: # added for Utilities.bpm (vimaier)
-    sys.path.append('/afs/cern.ch/eng/sl/lintrack/Beta-Beat.src/')
+
 import os
 import math
 
---
+++ GetLLM/__init__.py
@@ -0,0 +1,6 @@
+import sys
+import os
+
+# path_to_beta_beat = 
+# if "/afs/cern.ch/eng/sl/lintrack/Beta-Beat.src/" not in sys.path: # added for Utilities.bpm (vimaier)
+#     sys.path.append('/afs/cern.ch/eng/sl/lintrack/Beta-Beat.src/')
