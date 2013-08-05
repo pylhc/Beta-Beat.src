275177f88dd547526fdfe1e5b50345744421731c
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2013-08-05 17:35:21 +0200

    make sure test is failing if subprocess throws an error

--- tests/CTestTestfile.cmake
+++ tests/CTestTestfile.cmake
@@ -2,5 +2,8 @@ add_test(getsuper ${CTEST_SOURCE_DIRECTORY}/tests/getsuper
 		  ${CTEST_SOURCE_DIRECTORY}
 		  ${CTEST_SOURCE_DIRECTORY}/tests/models/LHCB1/1_10_1_3)
 
-set_tests_properties(getsuper PROPERTIES TIMEOUT 1200)
+set_tests_properties(getsuper PROPERTIES
+   TIMEOUT 1200
+   FAIL_REGULAR_EXPRESSION "(Value|Type|ZeroDivision|Name)Error"
+   )
 
