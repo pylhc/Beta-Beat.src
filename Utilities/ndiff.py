461cf13a60e376c3db2d137b309570b8771954b1
vimaier <Viktor.Maier@cern.ch>
2013-10-09 16:57:01 +0200

    Added function to compare tfs_files without whitespace

--- Utilities/ndiff.py
+++ Utilities/ndiff.py
@@ -38,7 +38,7 @@ def compare_dirs_with_files_matching_regex_list(dir1, dir2, regex_list=None, fil
         'file_to_config_file_dict': dict
             Keys are filenames(without path) and corresponding values are ndiff config filepaths.
             If File is not in dict a default config file will be used.
-                
+
         :Return: boolean
             Returns True if dirs are equal, otherwise false
             (exit_code, std_stream, err_stream)
@@ -50,9 +50,9 @@ def compare_dirs_with_files_matching_regex_list(dir1, dir2, regex_list=None, fil
         regex_list = []
     if file_to_config_file_dict is None:
         file_to_config_file_dict = {}
-        
+
     dir1_items = sorted(os.listdir(dir1))
-    
+
     for item in dir1_items:
         item1 = os.path.join(dir1, item)
         item2 = os.path.join(dir2, item)
@@ -78,6 +78,21 @@ def empty_list_or_str_matches_regex_list(file_str, regex_list):
             return True
     return False
 
+def compare_tfs_files_and_ignore_whitespace(file_a, file_b):
+    """
+    Returns true, if files are equal with the default config. Whitespace will not be compared.
+    Otherwise false, and output from ndiff will be printed.
+    """
+    ignore_whitespace_option = {"--blank":""}
+
+    (exit_code, std_stream, err_stream) = run_ndiff(file_a, file_b, ignore_whitespace_option)
+
+    if 0 == exit_code and ndiff_files_were_equal(err_stream):
+        return True
+    else:
+        print std_stream
+        print >> sys.stderr, err_stream
+
 def compare_tfs_files_and_ignore_header(file_a, file_b):
     """
     Returns true, if files are equal based on the ignore_tfs_header.cfg file.
@@ -85,24 +100,24 @@ def compare_tfs_files_and_ignore_header(file_a, file_b):
     """
     tfs_ignore_header_cfg = os.path.join(get_path_to_root_of_ndiff(), IGNORE_TFS_HEADER_CFG)
     ignore_whitespace_option = {"--blank":""}
-    
-    (exit_code, std_stream, err_stream) = run_ndiff(file_a, file_b, tfs_ignore_header_cfg, 
+
+    (exit_code, std_stream, err_stream) = run_ndiff(file_a, file_b, tfs_ignore_header_cfg,
                                                     ignore_whitespace_option)
-    
+
     if 0 == exit_code and ndiff_files_were_equal(err_stream):
         return True
     else:
         print std_stream
         print >> sys.stderr, err_stream
-    
-    
+
+
 
 
 def run_ndiff(file_a, file_b, config_file="", options_dict=None ):
     """
     Runs ndiff in a new subprocess to compare given files (a and b) with the config_file and the
     options_dict.
-    
+
     :Parameters:
         'file_a': string
             Path to file a
@@ -113,67 +128,67 @@ def run_ndiff(file_a, file_b, config_file="", options_dict=None ):
         'options_dict': dictionary option_letter --> argument
             A dictionary with the options.
             E.g.: a_dict = {"-d":"", "-a":"xyz"}
-                
+
         :Return: tupel(int, string, string)
             Returns the exit code and the outputstreams of the subprocess.
             (exit_code, std_stream, err_stream)
-    """    
+    """
     options_string = __parse_options_dict(options_dict)
-    
+
     if "" == config_file:
         config_file = get_path_to_default_config_file()
-    
-    
+
+
     call_command = [get_os_dependent_path_to_ndiff(), options_string, file_a, file_b, config_file]
-    
+
     process = subprocess.Popen(call_command,
-                       stdout=subprocess.PIPE, 
+                       stdout=subprocess.PIPE,
                        stderr=subprocess.PIPE)
 
     # wait for the process to terminate
     (std_stream, err_stream) = process.communicate()
     exit_code = process.returncode
-    
+
     return (exit_code, std_stream, err_stream)
-    
-    
-    
-    
+
+
+
+
 def __parse_options_dict(options_dict):
     if options_dict is None:
         return ""
-    
+
     result = []
     for key in options_dict:
         result.append(key)
         if "" != options_dict[key]:
             result.append(options_dict[key])
     return " ".join(result)
-    
+
 
 def get_path_to_default_config_file():
     """ Returns "x/Beta-Beat.src/binaries/ndiff/default.cfg" """
     return os.path.join(get_path_to_root_of_ndiff(), DEFAULT_CFG)
-    
-    
+
+
 def get_os_dependent_path_to_ndiff():
     path_to_ndiff_root = get_path_to_root_of_ndiff()
-    
+
     if "posix" == os.name:
         tail = os.path.join("linux","ndiff-linux64")
     elif "nt" == os.name:
         tail = os.path.join("windows","ndiff-win32.exe")
     else:
         raise OSError("ndiff only available for Linux and Windows. Your OS: "+os.name)
-    
+
     return os.path.join(path_to_ndiff_root,tail)
 
 
 def get_path_to_root_of_ndiff():
-    return os.path.join(Utilities.iotools.get_absolute_path_to_betabeat_root(), 
+    return os.path.join(Utilities.iotools.get_absolute_path_to_betabeat_root(),
                                       "binaries", "ndiff")
-    
-    
+
+
 def ndiff_files_were_equal(ndiff_output_string):
     """ Returns True if ndiff_output_string did not print a warning. """
     return "warng" not in ndiff_output_string
