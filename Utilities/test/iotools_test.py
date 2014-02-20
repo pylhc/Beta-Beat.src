3cb4d6b36cc6edc1f8b80af7dff0ba9729e3a503
vimaier <Viktor.Maier@cern.ch>
2014-02-20 16:28:28 +0100

    Sorted received lists
    Caused sometimes trouble since the lists are not always sorted

--- Utilities/test/iotools_test.py
+++ Utilities/test/iotools_test.py
@@ -8,6 +8,7 @@ import os
 
 import Utilities.iotools
 
+
 class TestReplacingKeywords(unittest.TestCase):
     """ tests function Utilities.iotools.replace_keywords_in_textfile """
 
@@ -28,66 +29,63 @@ value_for_key_a == value_for_key_a
 2345.212300 == 2345.212300
 """
         self.__dict_for_replacing = {
-                        "key_a" : "value_for_key_a",
-                        "key_b" : 2345.2123,
-                        "key_c" : 2345.2123,
-                        "key_d" : 2345.2123
+                        "key_a": "value_for_key_a",
+                        "key_b": 2345.2123,
+                        "key_c": 2345.2123,
+                        "key_d": 2345.2123
         }
         f_name = "replacing_test.txt"
         Utilities.iotools.write_string_into_new_file(f_name, self.__text_before_replacing)
         self.__created_files.append(f_name)
         self.__created_files.append("replaced_test.txt")
 
-
     def tearDown(self):
         for f_name in self.__created_files:
             Utilities.iotools.delete_item(f_name)
-
 
     def test_replacing(self):
         assumed_lines = self.__text_after_replacing
 
         Utilities.iotools.replace_keywords_in_textfile("replacing_test.txt", self.__dict_for_replacing, "replaced_test.txt")
         all_lines = Utilities.iotools.read_all_lines_in_textfile("replaced_test.txt")
-        self.assertTrue(all_lines == assumed_lines, "Strings are not equal:\n"+all_lines+assumed_lines)
+        self.assertTrue(all_lines == assumed_lines, "Strings are not equal:\n" + all_lines + assumed_lines)
 
         # Tests replacing in same file
         Utilities.iotools.replace_keywords_in_textfile("replacing_test.txt", self.__dict_for_replacing)
         all_lines = Utilities.iotools.read_all_lines_in_textfile("replacing_test.txt")
-        self.assertTrue(all_lines == assumed_lines, "Strings are not equal:\n"+all_lines+assumed_lines)
+        self.assertTrue(all_lines == assumed_lines, "Strings are not equal:\n" + all_lines + assumed_lines)
 
 
 class TestGetFilenamesInDir(unittest.TestCase):
-    """ tests function Utilities.iotools.replace_keywords_in_textfile """
 
     def setUp(self):
-        self.__list_of_abs_filepaths = []
-        self.__abs_path_to_root = os.path.join(os.path.dirname(os.path.abspath(__file__)),"root_for_test")
+        self.__assumed_list_of_abs_filepaths = []
+        self.__abs_path_to_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_for_test")
         self.__create_dirs_and_files()
 
     def tearDown(self):
         self.__delete_created_dirs_and_files()
 
-
     def test_filenames(self):
-        all_abs_filenames = Utilities.iotools.get_all_absolute_filenames_in_dir_and_subdirs(self.__abs_path_to_root)
-        self.assertEqual(all_abs_filenames, self.__list_of_abs_filepaths,
-                         "Abs. filenames are not equal:\n"+str(all_abs_filenames)+"\n"+str(self.__list_of_abs_filepaths))
+        received_all_abs_filenames = Utilities.iotools.get_all_absolute_filenames_in_dir_and_subdirs(self.__abs_path_to_root)
+        received_all_abs_filenames.sort()
+        self.assertEqual(received_all_abs_filenames, self.__assumed_list_of_abs_filepaths,
+                         "Abs. filenames are not equal:\n" + str(received_all_abs_filenames) + "\n" + str(self.__assumed_list_of_abs_filepaths))
 
-        all_filenames = Utilities.iotools.get_all_filenames_in_dir_and_subdirs(self.__abs_path_to_root)
+        received_filenames = Utilities.iotools.get_all_filenames_in_dir_and_subdirs(self.__abs_path_to_root)
+        received_filenames.sort()
         assumed_filenames = ["file1", "file2", "file3", "file4"]
-        self.assertEqual(all_filenames, assumed_filenames,
-                         "Filenames are not equal:\n"+str(all_filenames)+"\n"+str(assumed_filenames))
-
+        self.assertEqual(received_filenames, assumed_filenames,
+                         "Filenames are not equal:\n" + str(received_filenames) + "\n" + str(assumed_filenames))
 
     def __create_dirs_and_files(self):
-        self.__list_of_abs_filepaths.append(os.path.join(self.__abs_path_to_root, "file1"))
-        self.__list_of_abs_filepaths.append(os.path.join(self.__abs_path_to_root, "file2"))
-        self.__list_of_abs_filepaths.append(os.path.join(self.__abs_path_to_root, "subdir1", "file3"))
-        self.__list_of_abs_filepaths.append(os.path.join(self.__abs_path_to_root, "subdir1", "file4"))
+        self.__assumed_list_of_abs_filepaths.append(os.path.join(self.__abs_path_to_root, "file1"))
+        self.__assumed_list_of_abs_filepaths.append(os.path.join(self.__abs_path_to_root, "file2"))
+        self.__assumed_list_of_abs_filepaths.append(os.path.join(self.__abs_path_to_root, "subdir1", "file3"))
+        self.__assumed_list_of_abs_filepaths.append(os.path.join(self.__abs_path_to_root, "subdir1", "file4"))
         Utilities.iotools.create_dirs(os.path.join(self.__abs_path_to_root, "subdir1"))
         Utilities.iotools.create_dirs(os.path.join(self.__abs_path_to_root, "subdir2"))
-        for abs_file_name in self.__list_of_abs_filepaths:
+        for abs_file_name in self.__assumed_list_of_abs_filepaths:
             with open(abs_file_name, "w") as new_file:
                 new_file.write("Hello file!")
 
