d29e7649d019a2e93826c133f3dbe2903df08e7b
jaimecp89 <jaimecp89@gmail.com>
2014-04-14 14:03:03 +0200

    Updated the CTestTestfile.cmake of the drive test to use the new columns test.

--- drive/test/CTestTestfile.cmake
+++ drive/test/CTestTestfile.cmake
@@ -1,2 +1,2 @@
-add_test(drive_test_output ${PYTHON} ${CTEST_SOURCE_DIR}/drive/test/test_output.py)
+add_test(drive_test_output ${PYTHON} ${CTEST_SOURCE_DIR}/drive/test/test_output_for_new_columns.py)
 set_tests_properties(drive_test_output PROPERTIES TIMEOUT 900)
