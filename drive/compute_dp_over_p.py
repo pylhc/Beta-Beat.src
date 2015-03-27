af60e95f6c6d116d9264fb7d35fb29459db6f8da
jaimecp89 <jaimecp89@gmail.com>
2015-03-27 15:23:40 +0100

    Fixed the delta p over p string parsing again.

--- drive/compute_dp_over_p.py
+++ drive/compute_dp_over_p.py
@@ -4,6 +4,7 @@ from Python_Classes4MAD import metaclass
 import os
 import sys
 import json
+import re
 
 
 def _parse_args():
@@ -32,22 +33,31 @@ def compute_dp_over_p(model, files, output_file):
 
 
 def _single_file_dp_over_p(lin_x_twiss, model_twiss):
+    numer = 0
+    denom = 0
     for bpm_name in lin_x_twiss.NAME:
-        numer = 0
-        denom = 0
         if bpm_name in model_twiss.NAME:
-            dispersion = model_twiss.DX[model_twiss.indx[bpm_name]] * 1e3  # We need it in mm
-            closed_orbit = lin_x_twiss.CO[lin_x_twiss.indx[bpm_name]]
-            numer += dispersion * closed_orbit
-            denom += dispersion ** 2
+            if _is_arc_bpm(bpm_name):
+                dispersion = model_twiss.DX[model_twiss.indx[bpm_name]] * 1e3  # We need it in mm
+                closed_orbit = lin_x_twiss.CO[lin_x_twiss.indx[bpm_name]]
+                numer += dispersion * closed_orbit
+                denom += dispersion ** 2
     dp_over_p = numer / denom
     return dp_over_p
+
+
+def _is_arc_bpm(bpm_name):
+    match = re.search("BPM.*\.([0-9]+)[RL].\..*", bpm_name, re.IGNORECASE)
+    if match:
+        return int(match.group(1)) > 14  # The arc bpms are from BPM.14... and up
+    else:
+        return False
 
 
 def _parse_twiss_files(model, files):
     model_twiss = metaclass.twiss(model)
     lin_x_twiss_list = []
-    file_list = [file_name.strip() for file_name in files.split(",")]
+    file_list = [file_name.strip() for file_name in files.strip("\"").split(",")]
     for file_name in file_list:
         if not file_name.endswith("_linx"):
             file_name += "_linx"
@@ -61,6 +71,7 @@ def _parse_twiss_files(model, files):
 
 def _json_output(file_to_dpp_dict, output_file):
     json_string = json.dumps(file_to_dpp_dict)
+    print json_string
     with open(output_file, "w") as output_file_data:
         output_file_data.write(json_string)
 
