1a5984695b4b9cb746a1063a3ec845a616826359
jaimecp89 <jaimecp89@gmail.com>
2015-10-16 14:10:40 +0200

    Improved the ESRF converter.
    Now it will ignore BPMs with NaN values or those who have values
    repeated from the previous file.

--- MODEL/ESRF/esrf_sdds_converter.py
+++ MODEL/ESRF/esrf_sdds_converter.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import datetime
+import math
 from optparse import OptionParser
 
 
@@ -34,10 +35,16 @@ def main(input_file_path):
 
     bpm_data = json.load(open(os.path.join(CURRENT_PATH, "bpm_names.json"), "r"))
 
+    previous_file_tbt_data = None
+    this_file_tbt_data = None
     for set_number in range(num_sets_x):
         set_string = str(set_number + 1) if len(str(set_number + 1)) > 1 else "0" + str(set_number + 1)
         output_file_name = os.path.basename(input_file_path).replace(".mat", "." + set_string + '.sdds')
         output_file_path = os.path.join(os.path.dirname(input_file_path), output_file_name)
+        previous_file_tbt_data = this_file_tbt_data
+        this_file_tbt_data = {}
+        this_file_tbt_data["0"] = {}
+        this_file_tbt_data["1"] = {}
         with open(output_file_path, "w") as output_file:
             output_file.write("#SDDSASCIIFORMAT v1 \n")
             output_file.write("#Beam: ESRFonly1 \n")
@@ -47,18 +54,37 @@ def main(input_file_path):
             output_file.write("#NTURNS: " + str(num_turns_x) + "\n")
 
             for bpm_number in range(num_bpms_x):
-                _write_sdds_data_line(bpm_number, set_number, all_x, "0", bpm_data, output_file)
+                _write_sdds_data_line(bpm_number, set_number, all_x, "0", bpm_data, output_file,
+                                      this_file_tbt_data, previous_file_tbt_data)
 
             for bpm_number in range(num_bpms_y):
-                _write_sdds_data_line(bpm_number, set_number, all_y, "1", bpm_data, output_file)
+                _write_sdds_data_line(bpm_number, set_number, all_y, "1", bpm_data, output_file,
+                                      this_file_tbt_data, previous_file_tbt_data)
 
 
-def _write_sdds_data_line(bpm_number, set_number, all_data, plane_string, bpm_data, output_file):
+def _write_sdds_data_line(bpm_number, set_number, all_data, plane_string, bpm_data, output_file, this_file_tbt_data, previous_file_tbt_data):
     bpm_name = bpm_data["names"][bpm_number].replace("PICK", "BPM")
     bpm_s = bpm_data["s"][bpm_number]
     output_file.write(plane_string + " " + bpm_name + " " + str(bpm_s) + " ")
-    output_file.write(" ".join(map(str, map(lambda x: "{0:f}".format(x), all_data[:, bpm_number, set_number]))))
-    output_file.write("\n")
+    tbt_data = map(float, map(lambda x: "{0:f}".format(x), all_data[:, bpm_number, set_number]))
+    if _check_tbt_data(bpm_name, plane_string, tbt_data, previous_file_tbt_data):
+        this_file_tbt_data[plane_string][bpm_name] = tbt_data
+        output_file.write(" ".join(map(str, tbt_data)))
+        output_file.write("\n")
+
+
+def _check_tbt_data(bpm_name, plane_string, tbt_data, previous_file_tbt_data):
+    if any(map(math.isnan, tbt_data)):
+        print "Ignoring BPM", bpm_name, "found NaNs for plane", plane_string
+        return False
+    elif previous_file_tbt_data is None:
+        return True
+    else:
+        if bpm_name in previous_file_tbt_data[plane_string]:
+            if previous_file_tbt_data[plane_string][bpm_name] == tbt_data:
+                print "Ignoring BPM", bpm_name, "found repeated values from previous file for plane", plane_string
+                return False
+    return True
 
 
 def _launch_with_afs_anaconda(input_file_path):
