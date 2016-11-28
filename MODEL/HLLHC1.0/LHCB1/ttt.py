3245ae98c0fc3581784f4936a46961b2a3996d4a
Elena FOL <efol@pcbe16218.cern.ch>
2016-11-28 18:45:11 +0100

    Changed templates for HLLHC and added corresponding strenghts files and json files.

---
+++ MODEL/HLLHC1.0/LHCB1/ttt.py
@@ -0,0 +1,23 @@
+
+
+
+
+execfile('AllLists.py')
+
+Common, B1 = getListsByIR()
+
+print Common['7']
+
+print B1['7']
+
+
+
+
+
+execfile('../LHCB2/AllLists.py')
+
+Common, B2 = getListsByIR()
+
+            
+    
+print B2['7']
