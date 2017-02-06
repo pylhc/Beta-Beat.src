ed1e62ff500a9967cb9decbb94492f22678645b8
awegsche <a.wegscheider7141@gmail.com>
2017-02-06 17:34:38 +0100

    ADDbpmerrors updated
    - added argument to specify turn range. This allows to cut the ramp of an AC-dipole
    - added argument modeltwiss. If not None, it searches for the right S of the BPM.
    
    IMPORTANT: before it took the WRONG S in case the tracking didn't start at S=0.

--- Utilities/ADDbpmerror.py
+++ Utilities/ADDbpmerror.py
@@ -10,7 +10,8 @@ Created April, 2014
 from __future__ import print_function
 from numpy.random import normal, randint
 
-def convert_files( nparticles=1, infile='trackone', outfile='ALLBPMs', x_error=0.0, y_error=0.0, n_faulty=0):
+def convert_files( nparticles=1, infile='trackone', outfile='ALLBPMs',
+                   x_error=0.0, y_error=0.0, n_faulty=0., turnrange=None, modeltwiss=None):
     '''
     Similar to the old awk script ALLBPMs.
 
@@ -38,6 +39,7 @@ def convert_files( nparticles=1, infile='trackone', outfile='ALLBPMs', x_error=0
     bpms=[]
     bpm_loc=[]
 
+
     for l in file(infile):
         if l.strip()[0] in ['@','*','$']:
             continue
@@ -49,16 +51,17 @@ def convert_files( nparticles=1, infile='trackone', outfile='ALLBPMs', x_error=0
                         x[i][bpm_name]=[]
                         y[i][bpm_name]=[]
         elif 'BPM' in bpm_name or 'PICK' in bpm_name:  # PICK for ESRF
-            pid=int(lsp[0])-1
-            if bpm_name not in bpms:
-                bpms.append(bpm_name)
-                bpm_loc.append(lsp[-2])
-            if x_error and y_error:
-                x[pid][bpm_name].append(str(float(lsp[2]) * 1000 + normal(0.0, x_error)))
-                y[pid][bpm_name].append(str(float(lsp[4]) * 1000 + normal(0.0, y_error)))
-            else:
-                x[pid][bpm_name].append(str(float(lsp[2]) * 1000))
-                y[pid][bpm_name].append(str(float(lsp[4]) * 1000))
+                pid = int(lsp[0]) - 1
+                if bpm_name not in bpms:
+                    bpms.append(bpm_name)
+                    bpm_loc.append(lsp[-2])
+                if x_error and y_error:
+                    x[pid][bpm_name].append(str(float(lsp[2]) * 1000 + normal(0.0, x_error)))
+                    y[pid][bpm_name].append(str(float(lsp[4]) * 1000 + normal(0.0, y_error)))
+                else:
+                    x[pid][bpm_name].append(str(float(lsp[2]) * 1000))
+                    y[pid][bpm_name].append(str(float(lsp[4]) * 1000))
+
 
     if n_faulty:
         failing_bpms=randint(0,len(bpms)*2,n_faulty)
@@ -74,11 +77,27 @@ def convert_files( nparticles=1, infile='trackone', outfile='ALLBPMs', x_error=0
         fout.write('# title\n')
         for i in xrange(len(bpms)):
             bpm=bpms[i]
-            s=bpm_loc[i]
+            if modeltwiss is not None:
+                s = modeltwiss.S[modeltwiss.indx[bpm]]
+            else:
+                s=bpm_loc[i]
+
             if i not in failing_bpms:
-                fout.write(' '.join(['0',bpm,s]+x[pid][bpm])+'\n')
+                if turnrange is None:
+                    fout.write(' '.join(['0',bpm,s]+x[pid][bpm])+'\n')
+                else:
+                    fout.write('0 {0:s} {1:s} '.format(bpm, s))
+                    for j in range(turnrange[0], turnrange[1]):
+                        fout.write(str(x[pid][bpm][j]) + " ")
+                    fout.write("\n")
             if i+len(bpms) not in failing_bpms:
-                fout.write(' '.join(['1',bpm,s]+y[pid][bpm])+'\n')
+                if turnrange is None:
+                    fout.write(' '.join(['1',bpm,s]+x[pid][bpm])+'\n')
+                else:
+                    fout.write('1 {0:s} {1:s} '.format(bpm, s))
+                    for j in range(turnrange[0], turnrange[1]):
+                        fout.write(str(x[pid][bpm][j]) + " ")
+                    fout.write("\n")
 
 
 if __name__=="__main__":
