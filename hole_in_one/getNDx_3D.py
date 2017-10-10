Fixed the sign of synchrotron phase wrt arcs

hole_in_one/getNDx_3D.py
@@ -14,6 +14,7 @@ from optparse import OptionParser
 import time
 import numpy as np
 import pandas as pd
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from Utilities import tfs_pandas as tfs
 from model import manager
 
@@ -73,7 +74,7 @@ def getNDX(files,model,output):
             cols.append(d)
     #resolving the sign of dispersion
     for c in cols:
-        results[c.replace('scMUZ','fNDX')]=results.loc[:,c.replace('MU','AMP')] * np.sign(0.25 - np.mean(results.loc[:,cols],axis=1)) 
+        results[c.replace('scMUZ','fNDX')]=results.loc[:,c.replace('MU','AMP')] * np.sign(0.25 - np.abs(np.angle(np.sum(np.exp(PI2I * results.loc[:,cols]),axis=1)))/(2*np.pi))#np.mean(results.loc[:,cols],axis=1)) 
         columns.append(c.replace('scMUZ','fNDX'))
     forfile=results.loc[:,columns]
     f=[]