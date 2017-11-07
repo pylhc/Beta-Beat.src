26164cd0a976b9a8538f21f36084c576538e886b
Joschua <Joschua Dilly>
2017-11-07 19:14:07 +0100

    Same changes as about 40 commits back in hole_in_one/harpy.py

--- hole_in_one/harpy.py
+++ hole_in_one/harpy.py
@@ -347,17 +347,13 @@ def _search_highest_coefs(freq, tolerance, frequencies, coefficients):
     freq_vals = frequencies.values
     coefs_vals = coefficients.values
     on_window_mask = (freq_vals >= min_val) & (freq_vals <= max_val)
-    filtered_amps = np.where(on_window_mask,
-                             np.abs(coefs_vals),
-                             np.zeros_like(coefs_vals))
-    filtered_coefs = np.where(on_window_mask,
-                              coefs_vals,
-                              np.zeros_like(coefs_vals))
+    filtered_coefs = np.where(on_window_mask, coefs_vals, 0)
+    filtered_amps = np.abs(filtered_coefs)
     max_indices = np.argmax(filtered_amps, axis=1)
     max_coefs = filtered_coefs[np.arange(coefs_vals.shape[0]), max_indices]
     max_coefs = pd.Series(index=coefficients.index, data=max_coefs)
     max_freqs = freq_vals[np.arange(freq_vals.shape[0]), max_indices]
-    max_freqs = (max_coefs != 0) * max_freqs  # Zero bad freqs
+    max_freqs = pd.Series(index=coefficients.index, data=np.where(max_coefs != 0, max_freqs, 0))
     return max_coefs, max_freqs
 
 
@@ -417,7 +413,6 @@ def _compute_resonances_freqs(plane, tunes):
     constante RESONANCE_LISTS, together with the natural tunes
     frequencies if given.
     """
-    resonances_freqs = {}
     tunex, tuney, tunez = tunes
     freqs = [(resonance_h * tunex) +
              (resonance_v * tuney) +
@@ -455,7 +450,7 @@ def _laskar_method(tbt, num_harmonics):
     return frequencies, coefficients
 
 
-def _jacobsen(dft_values,n):
+def _jacobsen(dft_values, n):
     """
     This method interpolates the real frequency of the
     signal using the three highest peaks in the FFT.
