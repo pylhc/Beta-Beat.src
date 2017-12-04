007de045967c2963abe60cbff22ba794f34ddd1c
Joschua <Joschua Dilly>
2017-12-04 10:43:41 +0100

    Updated Sequence Parser
    - twiss_optics/sequence_parser.py
      * Updated to non-sliced sequences
      * takes length-multipliers into account (variables and from slicing)

--- twiss_optics/sequence_parser.py
+++ twiss_optics/sequence_parser.py
@@ -7,80 +7,129 @@ from Utilities import tfs_pandas as tfs
 LOG = logtool.get_logger(__name__)
 
 
+"""
+=============================   Main   =============================
+"""
+
+
 def parse_sequence_file(sequence_file):
     """
-    Create Mapping from knobs to magnets based on sequence_file.
+    Create Mapping from variables to magnets based on sequence_file.
      assumptions:
+     - Sequence is saved in a way, that magnets definitions contain 'knl:={}' or 'K#:='
      - we don't care about the N in KNL
-     - linearity: knobs do not multiply with each other (will result in zeros)
-     - the knob-name is final (i.e. it is not reassigned by ':=' somewhere else)
-     - the knob-name does not start with "l." (reserved for length multiplier)
-     - if there is a '(0.####)' at the end of the KNL it comes from splitting the element
+     - linearity: variables do not multiply with each other (will result in zeros)
+     - the variable-name is final (i.e. it is not reassigned by ':=' somewhere else)
+     - the variable-name does not start with "l." (reserved for length multiplier)
+     - apart from the length, there are no other named multipliers (numbers are fine)
 
     :param sequence_file: Saved Sequence file
-    :return: TFS_DF containing a matrix magnets vs. knobs containing the knob-coefficients
+    :return: TFS_DF containing a matrix -  magnets(index) vs. variables(columns)
+             containing the variable-coefficients
     """
     LOG.info("Parsing file '{:s}'".format(sequence_file))
 
     with timeit(lambda t:
                 LOG.debug("  File parsed in {:f}s".format(t))):
-        m_t_k = tfs.TfsDataFrame()
-        with open(sequence_file, 'r') as f_seq:
-            for line in f_seq:
-                match = re.search(r"knl:=\{(?P<knl>[^\}]+)\}", line)
-                if match is not None:
-                    magnet = re.match(r"([\w]|\.(?!\.))*", line).group(0)
-                    if magnet not in m_t_k.index:
-                        knls = match.group('knl').split(',')
-                        for n, knl in enumerate(knls):
-                            try:
-                                float(knl)  # check could also be "len(knl) > 1"
-                            except ValueError:
-                                # if one ever needs it: n is available as well!
-                                k_dict = _parse_knl_element(knl)
-                                m_t_k = m_t_k.append(
-                                    tfs.TfsDataFrame([k_dict.values()],
-                                                     index=[magnet],
-                                                     columns=k_dict.keys()
-                                                     ))
-    return m_t_k.fillna(0)
+        magnet_strings, length_constants = _read_file(sequence_file)
+        var_to_mag = _build_dataframe(magnet_strings, length_constants)
+    return var_to_mag
 
 
-def _parse_knl_element(knl):
-    """ Finds knob-names and coefficients in string 'knl' """
-    knl = knl.replace(" ", "")  # to simplify regex (no \s*)
+"""
+=============================   Read Sequence   =============================
+"""
 
-    # replace those splitting values by 1
-    num_split = re.search(r"\((0\.\d*)\)+$", knl)
-    if num_split is not None:
-        knl = _replace_chars_at(knl, num_split.start(1), num_split.end(1), "1")
 
-    # replace element-length multiplier by 1
-    el_length = re.search(r"l\.[\w.]+", knl)
+def _read_file(sequence_file):
+    """ Read the file and return """
+    length_constants = {}
+    magnet_strings = {}
+    with open(sequence_file, 'r') as f_seq:
+        for line in f_seq:
+            var_and_value = _find_length(line)
+            if var_and_value is not None:
+                length_constants[var_and_value[0]] = var_and_value[1]
+            else:
+                var_and_value = _find_strength(line)
+                if var_and_value is not None:
+                    magnet_strings[var_and_value[0]] = var_and_value[1]
+    return magnet_strings, length_constants
+
+
+def _find_length(line):
+    """ Search for length variable in line """
+    match = re.match(r"const\s(l\.[^;]+)", line)
+    if match is not None:
+        eq = match.group(1).replace(" ", "")
+        name_and_value = eq.split("=")
+        return name_and_value
+    else:
+        return None
+
+
+def _find_strength(line):
+    """ Search for magnet strengths in line """
+    match = re.search(r",\s*k(nl:=\{(?P<knl>[^\}]+)\}|\d:=(?P<k>[^,]+))", line)
+    if match is not None:
+        magnet = re.match(r"([\w]|\.)*", line).group(0)
+
+        if match.group("knl") is not None:
+            knls = match.group('knl').split(',')
+            for n, knl in enumerate(knls):
+                try:
+                    float(knl)  # check could also be "len(knl) > 1"
+                except ValueError:
+                    # if one ever needs it: n is available as well!
+                    return magnet, knl.replace(" ", "")
+        else:
+            return magnet, match.group("k").replace(" ", "")
+    else:
+        return None
+
+
+"""
+=============================   Build Mapping   =============================
+"""
+
+
+def _build_dataframe(magnet_strings, length_constants):
+    """ Build the data frame representing the mapping variables to magnets """
+    var_to_mag = tfs.TfsDataFrame()
+    for magnet, value_string in magnet_strings.iteritems():
+        k_dict = _eval_strength(value_string, length_constants)
+        var_to_mag = var_to_mag.append(
+            tfs.TfsDataFrame([k_dict.values()],
+                             index=[magnet],
+                             columns=k_dict.keys()
+                             ))
+    return var_to_mag.fillna(0)
+
+
+def _eval_strength(k_string, length_constants):
+    """ Evaluate the magnet-strength string and return dictionary with values """
+    # replace element-length multiplier by their value
+    el_length = re.search(r"l\.[\w.]+", k_string)
     if el_length is not None:
-        knl = knl.replace(el_length.group(0), "1")
+        l_var = el_length.group(0)
+        k_string = k_string.replace(l_var, length_constants[l_var])
 
-    # get knob names
-    knobs = re.findall(r"[a-zA-Z][\w.]*", knl)
+    # get variable names
+    variables = re.findall(r"[a-zA-Z][\w.]*", k_string)
     d = {}
-    for knob in knobs:
-        # replace current knob by 1 and all others by 0
-        knl_temp = knl
-        for other in knobs:
-            knl_temp = knl_temp.replace(other, "1" if knob == other else "0")
+    for variable in variables:
+        # replace current variable by 1 and all others by 0
+        knl_temp = k_string
+        for other in variables:
+            knl_temp = knl_temp.replace(other, "1" if variable == other else "0")
 
         # evaluate coefficient (let python sort out parenthesis, order etc.)
-        d[knob] = eval(knl_temp)
+        d[variable] = eval(knl_temp)
 
     return d
-
-
-def _replace_chars_at(s, start, end, replace=""):
-    """ kind of s[start:end] = replace, where replace can have any length """
-    return s[:start] + replace + s[end:]
 
 
 if __name__ == '__main__':
     import os
     df = parse_sequence_file(os.path.join("tests", "lhcb1.seq"))
-    print df
+    print df.loc['mq.26r5.b1..1', df.loc['mq.26r5.b1..1', :] != 0]
