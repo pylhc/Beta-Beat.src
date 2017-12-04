b316c46378710a28bd4203e67404d8c91ec36385
Joschua <Joschua Dilly>
2017-12-04 20:02:16 +0100

    New Class for Response Matrices. Please do not use yet. - twiss_optics/sequence_parser.py   * changed k0 parsing to angle-variables   * added function for automatic loading and saving of results
    - twiss_optics/optics_class.py
      * renamed and outsourced response matrix calculations
    
    - twiss_optics/response_class.py
      * new class to calculate response matrices
      * implemented phase-response (MU) calculations
    
    - twiss_optics/twiss_functions.py
      * methods used by TwissOptics and TwissResponse

--- twiss_optics/sequence_parser.py
+++ twiss_optics/sequence_parser.py
@@ -2,7 +2,8 @@ import re
 from Utilities import logging_tools as logtool
 from Utilities.contexts import timeit
 from Utilities import tfs_pandas as tfs
-
+import cPickle as pickle
+import pandas as pd
 
 LOG = logtool.get_logger(__name__)
 
@@ -12,29 +13,77 @@ LOG = logtool.get_logger(__name__)
 """
 
 
-def parse_sequence_file(sequence_file):
+def get_variable_mapping(sequence_file, ret="dictionary"):
     """
     Create Mapping from variables to magnets based on sequence_file.
      assumptions:
-     - Sequence is saved in a way, that magnets definitions contain 'knl:={}' or 'K#:='
-     - we don't care about the N in KNL (could be found quickly, though)
-     - linearity: variables do not multiply with each other (will result in zeros)
+     - Sequence is saved in a way, that magnets definitions contain 'knl:={}', 'ksl:={}' or 'K#:='
+     - There is only one value in each knl/ksl array.
+     - Dipole strengths (if not given as knl/ksl) are defined by their angle instead of k0
+     - zero-offset, i.e. no fixed number summation (see hint below)
+     - linearity, i.e. variables do not multiply with each other (will result in zeros)
      - the variable-name is final (i.e. it is not reassigned by ':=' somewhere else)
      - the variable-name does not start with "l." (reserved for length multiplier)
      - the length multiplier is given by l.<magnettype>
      - apart from the length, there are no other named multipliers (numbers are fine)
 
+     HINT: 'magnet := #.### + var' WILL NOT RESULT IN A VALID MAPPING !!!
+     (e.g. '1 + var' is indistinguishable from '2 * var', which is not what you want!)
+
     :param sequence_file: Saved Sequence file
-    :return: TFS_DF containing a matrix -  magnets(index) vs. variables(columns)
+    :param ret: return format, either "tfs" or "dictionary"
+    :return: TFS of magnets(index) vs. variables(columns)
              containing the variable-coefficients
+             or
+             Dictionary of all variables containing magnet-coefficient Series
     """
     LOG.info("Parsing file '{:s}'".format(sequence_file))
 
+    _check_ret(ret)
+
     with timeit(lambda t:
                 LOG.debug("  File parsed in {:f}s".format(t))):
-        magnet_strings, length_constants = _read_file(sequence_file)
-        var_to_mag = _build_dataframe(magnet_strings, length_constants)
-    return var_to_mag
+        magnet_strings, length_constants = _read_file_for_magnets(sequence_file)
+        if ret.lower() == "dataframe":
+            return _build_variable_mapping_df(magnet_strings, length_constants)
+        elif ret.lower() == "dictionary":
+            return _build_variable_mapping_dict(magnet_strings, length_constants)
+
+
+def load_or_calc_variable_mapping(seqfile_path, ret="dictionary"):
+    """
+    Convenience function for loading variable mapping from a file or calculating and saving
+    it if not found
+    :param seqfile_path: Saved Sequence file
+    :return:
+    """
+    _check_ret(ret)
+    varmapfile_path = seqfile_path.replace(".seq", "").replace(".varmap", "") + ".varmap"
+
+    try:
+        if ret == "dictionary":
+            with open(varmapfile_path, "rb") as varmapfile:
+                mapping = pickle.load(varmapfile)
+        elif ret == "dataframe":
+            mapping = tfs.read_tfs(varmapfile_path)
+        LOG.debug("Using mapping from varmap file '{:s}'.".format(varmapfile_path))
+
+    except IOError:
+        mapping = get_variable_mapping(seqfile_path, ret=ret)
+
+        try:
+            if ret == "dictionary":
+                with open(varmapfile_path, "wb") as varmapfile:
+                    pickle.dump(mapping, varmapfile, -1)
+            elif ret == "dataframe":
+                tfs.write_tfs(varmapfile_path, mapping, save_index=True)
+            LOG.debug("Saved Variable mapping into file '{:s}'".format(varmapfile_path))
+
+        except IOError as e:
+            LOG.debug("Could not save mapping to '{:s}',".format(varmapfile_path))
+            LOG.debug("  IOError: {:s}.".format(e.message))
+
+    return mapping
 
 
 """
@@ -42,23 +91,24 @@ def parse_sequence_file(sequence_file):
 """
 
 
-def _read_file(sequence_file):
+def _read_file_for_magnets(sequence_file):
     """ Read the file and return the magnet definitions and magnet lengths """
+    LOG.debug("  Reading File")
     length_constants = {}
     magnet_strings = {}
     with open(sequence_file, 'r') as f_seq:
         for line in f_seq:
-            var_and_value = _find_length(line)
+            var_and_value = _find_element_length(line)
             if var_and_value is not None:
                 length_constants[var_and_value[0]] = var_and_value[1]
             else:
-                var_and_value = _find_strength(line)
+                var_and_value = _find_magnet_strength(line)
                 if var_and_value is not None:
                     magnet_strings[var_and_value[0]] = var_and_value[1]
     return magnet_strings, length_constants
 
 
-def _find_length(line):
+def _find_element_length(line):
     """ Search for length variable in line """
     match = re.match(r"const\s(l\.[^;]+)", line)
     if match is not None:
@@ -69,11 +119,11 @@ def _find_length(line):
         return None
 
 
-def _find_strength(line):
+def _find_magnet_strength(line):
     """ Search for magnet strengths in line
         [If one ever needs n exchange '\d' for '(?P<N>\d) and use enumerate(knls)]
     """
-    match = re.search(r",\s*k(nl:=\{(?P<knl>[^\}]+)\}|\d:=(?P<k>[^,]+))", line)
+    match = re.search(r",\s*k([ns]l:=\{(?P<knl>[^\}]+)\}|(?P<n>\d):=(?P<k>[^,]+))", line)
     if match is not None:
         magnet = re.match(r"[\w.]*", line).group(0)
 
@@ -85,8 +135,12 @@ def _find_strength(line):
                 except ValueError:
                     return magnet, knl.replace(" ", "")
         else:
-            length = "l." + re.search(r":(?!=)\s*([^,]+)", line).group(1)
-            knl = "({kn:s}) * {l:s}".format(kn=match.group("k"), l=length)
+            if match.group("n") == '0':
+                # dipole strength are defined by their angles
+                knl = re.search(r"angle\s*:=\s*([^,]+)", line).group(1)
+            else:
+                length = "l." + re.search(r":(?!=)\s*([^,]+)", line).group(1)
+                knl = "({kn:s}) * {l:s}".format(kn=match.group("k"), l=length)
             return magnet, knl.replace(" ", "")
     else:
         return None
@@ -97,11 +151,14 @@ def _find_strength(line):
 """
 
 
-def _build_dataframe(magnet_strings, length_constants):
-    """ Build the data frame representing the mapping variables to magnets """
+def _build_variable_mapping_df(magnet_strings, length_constants):
+    """ Build the data frame representing the mapping variables to magnets and return as df
+        SLOW!!
+    """
+    LOG.debug("  Building Dataframe Mapping")
     var_to_mag = tfs.TfsDataFrame()
     for magnet, value_string in magnet_strings.iteritems():
-        k_dict = _eval_strength(value_string, length_constants)
+        k_dict = _eval_magnet_strength(value_string, length_constants)
         var_to_mag = var_to_mag.append(
             tfs.TfsDataFrame([k_dict.values()],
                              index=[magnet],
@@ -110,7 +167,23 @@ def _build_dataframe(magnet_strings, length_constants):
     return var_to_mag.fillna(0)
 
 
-def _eval_strength(k_string, length_constants):
+def _build_variable_mapping_dict(magnet_strings, length_constants):
+    """ Build the data frame representing the mapping variables to magnets and return as dict
+        Faster!
+    """
+    LOG.debug("  Building Dictionary Mapping")
+    var_to_mag = {}
+    for magnet, value_string in magnet_strings.iteritems():
+        k_dict = _eval_magnet_strength(value_string, length_constants)
+        for var in k_dict:
+            try:
+                var_to_mag[var].loc[magnet] = k_dict[var]
+            except KeyError:
+                var_to_mag[var] = pd.Series(k_dict[var], index=[magnet])
+    return var_to_mag
+
+
+def _eval_magnet_strength(k_string, length_constants):
     """ Evaluate the magnet-strength string and return dictionary with values """
     # replace element-length multiplier by their value
     el_length = re.search(r"l\.[\w.]+", k_string)
@@ -133,7 +206,23 @@ def _eval_strength(k_string, length_constants):
     return d
 
 
+"""
+=============================   Helper   =============================
+"""
+
+
+def _check_ret(ret):
+    ret_choices =["dataframe", "dictionary"]
+    if ret not in ret_choices:
+            ValueError("Return format '{ret:s}' unknown, choices: {choices:s}".format(
+                ret=ret, choices=", ".join(ret_choices)
+            ))
+
+
 if __name__ == '__main__':
-    import os
-    df = parse_sequence_file(os.path.join("tests", "lhcb1_tmp.seq"))
-    print df.loc['mq.26r5.b1', df.loc['mq.26r5.b1', :] != 0]
+    pass
+
+    # # Testing:
+    # import os
+    # df = get_variable_mapping(os.path.join("tests", "lhcb1_tmp.seq"))
+    # print df.loc['mq.26r5.b1', df.loc['mq.26r5.b1', :] != 0]
