c6e3958e222e3ab704a7827a66f87efc0c7166f0
Lukas Malina <lukas.malina@cern.ch>
2018-01-12 23:26:55 +0100

    Removed unused scripts

--- K-mod/TESTING/var4mad.sh
+++
@@ -1,4 +0,0 @@
-sed -e 's/%IP/'5'/g' \
-    -e 's/%MPATH/\/afs\/cern.ch\/eng\/sl\/lintrack\/Beta-Beat.src\/K-mod\//g' \
-    -e 's/%DELTAK/1e-5/g' \
-</afs/cern.ch/eng/sl/lintrack/Beta-Beat.src/K-mod/TESTING//job.statistics.mask > .//t.madx 
