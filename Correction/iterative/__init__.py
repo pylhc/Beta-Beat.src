645b7df98aa29e8cf38038bb8a53a5950a30b6e6
Joschua <Joschua Dilly>
2018-02-05 12:18:17 +0100

    Added some __init__ files

--- Correction/iterative/__init__.py
+++ Correction/iterative/__init__.py
@@ -6,3 +6,4 @@ new_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__
                                         os.path.pardir, os.path.pardir))
 if new_path not in sys.path:
     sys.path.append(new_path)
+
