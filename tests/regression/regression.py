429e20c28fec6b5d827f9c26cbb0c9ec9ad9077d
jaimecp89 <jaimecp89@gmail.com>
2018-02-11 16:56:00 +0100

    Added documentation and improved output.
    Also added compare_utils, that will contaion useful functions to
    compare directories.

--- tests/regression/regression.py
+++ tests/regression/regression.py
@@ -1,14 +1,17 @@
 from __future__ import print_function
 import sys
 import os
+import subprocess
 import traceback
+import contextlib
+import tempfile
+import shutil
 from collections import namedtuple
 import re
 import git
 
 
 _THIS_DIR = os.path.abspath(os.path.dirname(__file__))
-_TEST_DIRNAME = "_test_repo"
 _TEST_REGEXP = "^before.*$"
 
 
@@ -31,45 +34,81 @@ class TestCase(namedtuple(
     __slots__ = ()  # This makes the class lightweight and immutable.
 
 
-def launch_test_set(test_cases, repo_path):
+def launch_test_set(test_cases, repo_path, tag_regexp=_TEST_REGEXP):
     """
     """
-    test_rev = find_test_revision(repo_path, _TEST_REGEXP)
-    print("Testing against revision: {}".format(test_rev))
-    new_repo_path = os.path.join(_THIS_DIR, _TEST_DIRNAME)
-    print("Cloning repository at revision...")
-    clone_revision(repo_path, test_rev, new_repo_path)
-    print("Start test cases...")
+    test_tag = find_tag(repo_path, tag_regexp)
+    print("Testing against tag \"{tag}\":\n"
+          "    -> {tag.commit.summary} ({tag.commit.hexsha})"
+          .format(tag=test_tag))
+    with _temporary_dir() as new_repo_path:
+        print("Cloning repository into {}".format(new_repo_path))
+        clone_revision(repo_path, test_tag.commit.hexsha, new_repo_path)
+        print("\n################# Start test cases #################")
+        find_regressions(test_cases, new_repo_path, repo_path)
+
+
+def find_regressions(test_cases, valid_path, test_path):
+    """Test the directory test_path for regressions against valid_path.
+
+    It will print the results of each regression search test case in test_path,
+    firstly showing a summary like .FE, where '.' means valid, 'F' failed and
+    'E' errored. Afterwards it will show a more detailed result, showing
+    exception thrown and differences found.
+
+    Attributes:
+        test_cases: an iterable of TestCase to test the test_path.
+        valid_path: Path to the reference directory to test against.
+        test_path: Path to the directory to be tested for regressions.
+    """
     results = []
     for test_case in test_cases:
-        print("Test case: {}".format(test_case.name))
         try:
-            result = run_test_case(test_case, repo_path, new_repo_path)
-        except Exception as err:
-            result = err
-            print("E")
-        else:
-            print("." if result else "F")
+            result = TestResult(
+                result=run_test_case(test_case, valid_path, test_path),
+                is_error=False
+            )
+        # Sorry pylint, but I have to capture every exception:
+        except:  # pylint: disable=W0702
+            result = TestResult(
+                result=traceback.format_exc(),
+                is_error=True,
+            )
         results.append(result)
-    for idx, (result, test_case) in enumerate(zip(results, test_cases)):
-        report = "Test #{}: ".format(idx)
-        if isinstance(result, Exception):
-            report += "errored ->"
-            print(err)
-            continue
-        report += "succeeded" if result else "failed"
+        print(result.get_char(), end="")
+    print("\n\n################### Test results ###################")
+    report = ""
+    for result, test_case in zip(results, test_cases):
+        report += "Test {}: ".format(test_case.name)
+        if result.is_error:
+            report += "errored ->\n"
+            report += "{}".format(result.result)
+        else:
+            report += "succeeded" if result else "failed"
+        report += "\n\n"
+    print(report)
 
 
-def run_test_case(test_case, orig_repo_path, test_repo_path):
-    """Launch test_case and compare the results.
+def run_test_case(test_case, valid_path, test_path):
+    """Launch single test_case and compare the results.
+
+    Runs the given test, comparing the repositories at valid_path against
+    test_path.
+
+    Arguments:
+        test_case: The test case to run.
+        valid_path: The reference directory to compare against.
+        test_path: The test directory to check for regressions.
+    Returns:
+        Whatever 'test_case.test_function' returns, usually a boolean.
     """
-    orig_path = os.path.join(orig_repo_path, test_case.script)
-    test_path = os.path.join(test_repo_path, test_case.script)
-    _launch_command(orig_path, test_case.arguments)
-    _launch_command(test_path, test_case.arguments)
-    orig_outpath = os.path.join(orig_repo_path, test_case.output)
-    test_outpath = os.path.join(test_repo_path, test_case.output)
-    return test_case.test_function(orig_outpath, test_outpath)
+    valid_script = os.path.join(valid_path, test_case.script)
+    test_script = os.path.join(test_path, test_case.script)
+    _launch_command(valid_script, test_case.arguments)
+    _launch_command(test_script, test_case.arguments)
+    valid_outpath = os.path.join(valid_path, test_case.output)
+    test_outpath = os.path.join(test_path, test_case.output)
+    return test_case.test_function(valid_outpath, test_outpath)
 
 
 def clone_revision(source, revision, to_path):
@@ -95,12 +134,20 @@ def clone_revision(source, revision, to_path):
     new_repo = my_repo.clone(to_path)
     new_repo.head.reference = new_repo.commit(revision)
     assert new_repo.head.is_detached
+    new_repo.head.reset(index=True, working_tree=True)
     return new_repo
 
 
-def find_test_revision(repo, tag_regexp):
-    """
-    TODO
+def find_tag(repo, tag_regexp):
+    """Returns the most recent git tag that matches a regular expression.
+
+    Arguments:
+        repo: The Repo object, url or path to use.
+        tag_regexp: Regular expression to try and match in the tag names.
+    Returns:
+        The most recent tag in 'repo' that matches tag_regexp.
+    Raises:
+        TestError: if 'tag_regexp' doesn't match any tag in 'repo'.
     """
     repo = _force_repo(repo)
     matcher = re.compile(tag_regexp)
@@ -109,8 +156,34 @@ def find_test_revision(repo, tag_regexp):
                           reverse=True)
     for tag in ordered_tags:
         if matcher.findall(tag.name):
-            return tag.commit.hexsha
+            return tag
     raise TestError("Can't find a tag that matches {}".format(tag_regexp))
+
+
+class TestError(Exception):
+    """Raised when an error in the test configuration happens.
+    """
+    pass
+
+
+class TestResult(object):
+    """Hold the results of the tests.
+
+    Attributes:
+        result: The result of the test, either a boolean for success or fail,
+            or an string containing the stack trace of an Exception.
+        is_error: True only if the test errored.
+    """
+    __slots__ = ("result", "is_error", "get_char")
+
+    def __init__(self, result, is_error):
+        self.result = result
+        self.is_error = is_error
+
+    def get_char(self):
+        if self.is_error:
+            return "E"
+        return "." if self.result else "F"
 
 
 def _force_repo(repo):
@@ -120,12 +193,25 @@ def _force_repo(repo):
 
 
 def _launch_command(script, args):
-    python = sys.executable
-    os.system("{python} {script} {args}"
-              .format(python=python, script=script, args=args))
+    # TODO: Not sure about this... but easiest way of capturing exceptions.
+    with _temporary_args(args.split(" ")):
+        execfile(script)
 
 
-class TestError(Exception):
-    """Raised when an error in the test configuration happens.
-    """
-    pass
+@contextlib.contextmanager
+def _temporary_dir():
+    try:
+        dir_path = tempfile.mkdtemp()
+        yield dir_path
+    finally:
+        shutil.rmtree(dir_path)
+
+
+@contextlib.contextmanager
+def _temporary_args(args):
+    old_args = sys.argv
+    try:
+        sys.argv = args
+        yield
+    finally:
+        sys.argv = old_args
--- tests/regression/test_cases.py
+++ tests/regression/test_cases.py
@@ -1,13 +1,34 @@
+from __future__ import print_function
 import os
-from regression import TestCase, launch_test_set
+from regression import TestCase, launch_test_set, find_regressions
+import compare_utils
 
 
-TEST_CASES = (
-    TestCase("test_the_test",
-             "tests/regression/something.py", "5", "./_test_out/",
-             lambda dir1, dir2: False),
-)
+def get_test_cases():
+    return (
+        TestCase(name="should_error",
+                 script="tests/regression/something.py",
+                 arguments="5",
+                 output="tests/regression/_test_out/",
+                 test_function=compare_utils.compare_dirs_with),
+        TestCase(name="should_fail",
+                 script="tests/regression/something.py",
+                 arguments="5",
+                 output="tests/regression/_test_out/",
+                 test_function=lambda x, y: False),
+        TestCase(name="should_succeed",
+                 script="tests/regression/something.py",
+                 arguments="5",
+                 output="tests/regression/_test_out/",
+                 test_function=lambda x, y: True),
+    )
+
+
+def test_function(valid_path, test_path):
+    print("Compare valid: {} and test: {}.".format(valid_path, test_path))
+    raise NotImplementedError()
 
 
 if __name__ == "__main__":
-    launch_test_set(TEST_CASES, os.path.abspath("../.."))
+    launch_test_set(get_test_cases(), os.path.abspath("../.."))
+    # find_regressions(get_test_cases(), "./_test_repo", os.path.abspath("../.."))
