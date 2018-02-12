4e6d552549ac6673f11321b66b4365a64a8fc620
Joschua <Joschua Dilly>
2018-02-12 11:28:45 +0100

    Changed to entrypoint in creator

--- model/creator.py
+++ model/creator.py
@@ -1,11 +1,6 @@
 import __init__
-import sys
-import os
-import argparse
-from Utilities.entrypoint import split_arguments
-
 import manager  # noqa
-from model_creators import model_creator  # noqa
+from Utilities.entrypoint import EntryPointParameters, entrypoint
 from model_creators.lhc_model_creator import (  # noqa
     LhcModelCreator,
     LhcBestKnowledgeCreator,
@@ -13,7 +8,6 @@ from model_creators.lhc_model_creator import (  # noqa
     LhcCouplingCreator,
 )
 from model_creators.psbooster_model_creator import PsboosterModelCreator
-
 
 CREATORS = {
     "lhc": {"nominal": LhcModelCreator,
@@ -24,6 +18,52 @@ CREATORS = {
 }
 
 
+def _get_params():
+    params = EntryPointParameters()
+    params.add_argument(
+        flags=["--type"],
+        name="type",
+        help="Type of model to create, either nominal or best_knowledge",
+        choices=("nominal", "best_knowledge", "coupling_correction"),
+    )
+    params.add_argument(
+        flags=["--output"],
+        help="Output path for model, twiss files will be writen here.",
+        name="output",
+        required=True,
+        type=str,
+    )
+    params.add_argument(
+        flags=["--writeto"],
+        help="Path to the file where to write the resulting MAD-X script. ",
+        name="writeto",
+        type=str,
+    )
+    params.add_argument(
+        flags=["--logfile"],
+        help=("Path to the file where to write the MAD-X script output."
+              "If not provided it will be written to sys.stdout."),
+        name="logfile",
+        type=str,
+    )
+    return params
+
+
+# Main functions ###############################################################
+
+
+@entrypoint(_get_params())
+def create_instance_and_model(opt, accel_opt):
+    accel_inst = manager.get_accel_instance(accel_opt)
+    create_model(
+        accel_inst,
+        opt.type,
+        opt.output,
+        writeto=opt.writeto,
+        logfile=opt.logfile,
+    )
+
+
 def create_model(accel_inst, model_type, output_path, **kwargs):
     CREATORS[accel_inst.NAME][model_type].create_model(
         accel_inst,
@@ -32,49 +72,8 @@ def create_model(accel_inst, model_type, output_path, **kwargs):
     )
 
 
-def _i_am_main():
-    args = sys.argv[1:]
-    options, accel_args = _parse_creator_args(args)
-    accel_inst = manager.get_accel_instance(accel_args)
-    create_model(
-        accel_inst,
-        options.type,
-        options.output,
-        writeto=options.writeto,
-        logfile=options.logfile,
-    )
-
-
-def _parse_creator_args(args):
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "type",
-        help="Type of model to create, either nominal or best_knowledge",
-        choices=("nominal", "best_knowledge", "coupling_correction"),
-    )
-    parser.add_argument(
-        "--output",
-        help="Output path for model, twiss files will be writen here.",
-        dest="output",
-        required=True,
-        type=str,
-    )
-    parser.add_argument(
-        "--writeto",
-        help="Path to the file where to write the resulting MAD-X script. ",
-        dest="writeto",
-        type=str,
-    )
-    parser.add_argument(
-        "--logfile",
-        help=("Path to the file where to write the MAD-X script output."
-              "If not provided it will be written to sys.stdout."),
-        dest="logfile",
-        type=str,
-    )
-    options, unknown_args = parser.parse_known_args(args)
-    return options, unknown_args
+# Script Mode ##################################################################
 
 
 if __name__ == "__main__":
-    _i_am_main()
+    create_instance_and_model()
