c763e2773198df6a6ac5c451988fda543c5a700d
Joschua <Joschua Dilly>
2018-02-13 16:52:46 +0100

    #64: lhc.py from accelerator_class_for_beta: inits from modelfile

--- model/accelerators/lhc.py
+++ model/accelerators/lhc.py
@@ -4,10 +4,12 @@ import re
 import json
 from collections import OrderedDict
 import numpy as np
-from utils import tfs_pandas
-from accelerator import Accelerator, AcceleratorDefinitionError, Element
-from utils.entrypoint import EntryPoint, EntryPointParameters, split_arguments
+from Utilities import tfs_pandas
+from accelerator import Accelerator, AcceleratorDefinitionError, Element, AccExcitationMode, get_commonbpm
+from Utilities.entrypoint import EntryPoint, EntryPointParameters, split_arguments
+from Utilities import logging_tools
 
+LOGGER = logging_tools.get_logger(__name__)
 CURRENT_DIR = os.path.dirname(__file__)
 LHC_DIR = os.path.join(CURRENT_DIR, "lhc")
 
@@ -90,16 +92,20 @@ class Lhc(Accelerator):
     def get_instance_parameters():
         params = EntryPointParameters()
         params.add_parameter(
+            flags=["--model_dir", "-m"],
+            help="Path to model directory (loads tunes and excitation from model!).",
+            name="model_dir",
+            type=str,
+        )
+        params.add_parameter(
             flags=["--nattunex"],
             help="Natural tune X without integer part.",
-            required=True,
             name="nat_tune_x",
             type=float,
         )
         params.add_parameter(
             flags=["--nattuney"],
             help="Natural tune Y without integer part.",
-            required=True,
             name="nat_tune_y",
             type=float,
         )
@@ -144,7 +150,6 @@ class Lhc(Accelerator):
             flags=["--optics"],
             help="Path to the optics file to use (modifiers file).",
             name="optics",
-            required=True,
             type=str,
         )
         params.add_parameter(
@@ -168,35 +173,141 @@ class Lhc(Accelerator):
         # for reasons of import-order and class creation, decoration was not possible
         parser = EntryPoint(self.get_instance_parameters(), strict=True)
         opt = parser.parse(*args, **kwargs)
-        self.nat_tune_x = opt.nat_tune_x
-        self.nat_tune_y = opt.nat_tune_y
-        if opt.acd and opt.adt:
-            raise AcceleratorDefinitionError(
-                "Select only one excitation type."
-            )
-        if opt.acd:
-            self.excitation = LhcExcitationMode.ACD
-        elif opt.adt:
-            self.excitation = LhcExcitationMode.ADT
+
+        if opt.model_dir:
+            self.init_from_model_dir(opt.model_dir)
+            self.optics_file = None
+            self.energy = None
+            self.dpp = 0.0
+            self.xing = None
+            if opt.nat_tune_x is not None:
+                raise AcceleratorDefinitionError("Argument 'nat_tune_x' not allowed when loading from model directory.")
+            if opt.nat_tune_y is not None:
+                raise AcceleratorDefinitionError("Argument 'nat_tune_y' not allowed when loading from model directory.")
+            if opt.drv_tune_x is not None:
+                raise AcceleratorDefinitionError("Argument 'drv_tune_x' not allowed when loading from model directory.")
+            if opt.drv_tune_y is not None:
+                raise AcceleratorDefinitionError("Argument 'drv_tune_y' not allowed when loading from model directory.")
+            self.optics_file = opt.get("optics", os.path.join(os.path.dirname(opt.model_dir), "modifiers.madx"))
         else:
-            self.excitation = LhcExcitationMode.FREE
+            if opt.nat_tune_x is None:
+                raise AcceleratorDefinitionError("Argument 'nat_tune_x' is required.")
+            if opt.nat_tune_y is None:
+                raise AcceleratorDefinitionError("Argument 'nat_tune_y' is required.")
+            if opt.drv_tune_x is None:
+                raise AcceleratorDefinitionError("Argument 'drv_tune_x' is required.")
+            if opt.drv_tune_y is None:
+                raise AcceleratorDefinitionError("Argument 'drv_tune_x' is required.")
 
-        if opt.acd or opt.adt:
-            # "required"
-            self.drv_tune_x = opt.drv_tune_x
-            self.drv_tune_y = opt.drv_tune_y
+            self.nat_tune_x = opt.nat_tune_x
+            self.nat_tune_y = opt.nat_tune_y
+            if opt.acd and opt.adt:
+                raise AcceleratorDefinitionError(
+                    "Select only one excitation type."
+                )
+            if opt.acd:
+                self.excitation = LhcExcitationMode.ACD
+            elif opt.adt:
+                self.excitation = LhcExcitationMode.ADT
+            else:
+                self.excitation = LhcExcitationMode.FREE
 
-        # required
-        self.optics_file = opt.optics
+            if opt.acd or opt.adt:
+                # "required"
+                self.drv_tune_x = opt.drv_tune_x
+                self.drv_tune_y = opt.drv_tune_y
+            # optional with default
+            self.dpp = opt.dpp
+            self.fullresponse = opt.fullresponse
 
-        # optional with default
-        self.dpp = opt.dpp
-        self.fullresponse = opt.fullresponse
+            # optional no default
+            self.energy = opt.get("energy", None)
+            self.xing = opt.get("xing", None)
+            self.optics_file = opt.get("optics", None)
 
-        # optional no default
-        self.energy = opt.get("energy", None)
-        self.xing = opt.get("xing", None)
+            # for GetLLM
+            self.model = None
+            self.model_driven = None
+            self.model_best_knowledge = None
+            self.elements = None
+            self.elements_centre = None
+            self.model_dir = None
+            self.errordefspath = None
+
         self.verify_object()
+
+    def init_from_model_dir(self, model_dir):  # prints only for debugging
+        
+        LOGGER.info("=== ACCELERATOR CLASS FOR GETLLM ============")
+        
+        #LOGGER.info("class: {}".format(self.__name__))
+        LOGGER.info("Creating accelerator instance from model dir")
+
+
+       
+        self.model_dir = model_dir
+        
+        self.model_tfs = tfs_pandas.read_tfs(os.path.join(model_dir, "twiss.dat")).set_index("NAME")
+        LOGGER.info("  model path = " + os.path.join(model_dir, "twiss.dat"))
+            
+        self._excitation = AccExcitationMode.FREE
+        ac_filename = os.path.join(model_dir, "twiss_ac.dat")
+        adt_filename = os.path.join(model_dir, "twiss_adt.dat")
+        
+        if os.path.isfile(ac_filename):
+            self.model_driven = tfs_pandas.read_tfs(ac_filename).set_index("NAME")
+            self.excitation = AccExcitationMode.ACD
+            driven_filename = ac_filename
+            
+        if os.path.isfile(adt_filename):
+            if self.excitation == AccExcitationMode.ACD:
+                raise AcceleratorDefinitionError("ADT as well as ACD models provided. What do you want? Please come back to me once you have made up your mind.")
+
+            self.model_driven = tfs_pandas.read_tfs(adt_filename).set_index("NAME")
+            self.excitation = AccExcitationMode.ADT
+            driven_filename = adt_filename
+        
+        
+        model_best_knowledge_path = os.path.join(model_dir, "twiss_best_knowledge.dat")
+        if os.path.isfile(model_best_knowledge_path):
+            self.model_best_knowledge = tfs_pandas.read_tfs(model_best_knowledge_path).set_index("NAME")
+            LOGGER.info("{:20s} [{:>10s}]".format("Best Knowledge Model", "OK"))
+        else:
+            self.model_best_knowledge = None
+            LOGGER.info("{:20s} [{:>10s}]".format("Best Knowledge Model", "NO"))
+            
+        elements_path = os.path.join(model_dir, "twiss_elements.dat")
+        if os.path.isfile(elements_path):
+            self.elements = tfs_pandas.read_tfs(elements_path).set_index("NAME")
+        else:
+            raise AcceleratorDefinitionError("Elements twiss not found")
+        elements_path = os.path.join(model_dir, "twiss_elements_centre.dat")
+        if os.path.isfile(elements_path):
+            self.elements_centre = tfs_pandas.read_tfs(elements_path).set_index("NAME")
+        else:
+            self.elements_centre = self.elements
+        
+        self.drv_tune_x = float(self.get_driven_tfs().headers["Q1"])
+        self.drv_tune_y = float(self.get_driven_tfs().headers["Q2"])
+        self.nat_tune_x = float(self.model_tfs.headers["Q1"])
+        self.nat_tune_y = float(self.model_tfs.headers["Q2"])
+        
+        
+        LOGGER.info("{:20s} [{:>10s}]".format("Natural Tune X", "{:7.3f}".format(self.nat_tune_x)))
+        LOGGER.info("{:20s} [{:>10s}]".format("Natural Tune Y", "{:7.3f}".format(self.nat_tune_y)))
+    
+        if self._excitation == AccExcitationMode.FREE:
+            LOGGER.info("{:20s} [{:>10s}]".format("Excitation", "NO"))
+        else:
+            if self._excitation == AccExcitationMode.ACD:
+                LOGGER.info("{:20s} [{:>10s}]".format("Excitation", "ACD"))
+            elif self._excitation == AccExcitationMode.ADT:
+                LOGGER.info("{:20s} [{:>10s}]".format("Excitation", "ADT"))
+            LOGGER.info("{:20s} [{:>10s}]".format("> Driven Tune X", "{:7.3f}".format(self.drv_tune_x)))
+            LOGGER.info("{:20s} [{:>10s}]".format("> Driven Tune Y", "{:7.3f}".format(self.drv_tune_y)))
+          
+        
+        LOGGER.info("===")
 
     @classmethod
     def init_and_get_unknowns(cls, args=None):
@@ -300,15 +411,17 @@ class Lhc(Accelerator):
                 "The accelerator definition is incomplete, beam " +
                 "has to be specified (--beam option missing?)."
             )
-        if self.optics_file is None:
-            raise AcceleratorDefinitionError(
-                "The accelerator definition is incomplete, optics "
-                "file has not been specified."
-            )
+        if self.model_dir is None:  # is the class is used to create full response?
+            if self.optics_file is None:
+                raise AcceleratorDefinitionError(
+                    "The accelerator definition is incomplete, optics "
+                    "file or model directory has not been specified."
+                )
+            if self.xing is None:
+                raise AcceleratorDefinitionError("Crossing on or off not set.")
+
         if self.excitation is None:
             raise AcceleratorDefinitionError("Excitation mode not set.")
-        if self.xing is None:
-            raise AcceleratorDefinitionError("Crossing on or off not set.")
         if (self.excitation == LhcExcitationMode.ACD or
                 self.excitation == LhcExcitationMode.ADT):
             if self.drv_tune_x is None or self.drv_tune_y is None:
@@ -387,6 +500,120 @@ class Lhc(Accelerator):
             raise ValueError("Wrong excitation mode.")
         self._excitation = excitation_mode
 
+    # For GetLLM --------------------------------------------------------------
+     
+    def get_exciter_bpm(self, plane, commonbpms):
+        
+        if self.get_beam() == 1:
+            if self.excitation == AccExcitationMode.ACD:
+                return get_commonbpm("BPMYA.5L4.B1", "BPMYB.6L4.B1", commonbpms), "MKQA.6L4.B1"
+               
+            elif self.excitation == AccExcitationMode.ADT:
+                if plane == "H":
+                    return get_commonbpm("BPMWA.B5L4.B1", "BPMWA.A5L4.B1", commonbpms), "ADTKH.C5L4.B1"
+                elif plane == "V":
+                    return get_commonbpm("BPMWA.B5R4.B1", "BPMWA.A5R4.B1", commonbpms), "ADTKV.B5R4.B1"
+        elif self.get_beam() == 2:
+            if self.excitation == AccExcitationMode.ACD:
+                return get_commonbpm("BPMYB.5L4.B2", "BPMYA.6L4.B2", commonbpms), "MKQA.6L4.B2"
+            elif self.excitation == AccExcitationMode.ADT:
+                if plane == "H":
+                    return get_commonbpm("BPMWA.B5R4.B2", "BPMWA.A5R4.B2", commonbpms), "ADTKH.C5R4.B2"
+                elif plane == "V":
+                    return get_commonbpm("BPMWA.B5L4.B2", "BPMWA.A5L4.B2", commonbpms), "ADTKV.B5L4.B2"
+        return None
+    
+    def get_important_phase_advances(self):
+        if self.get_beam() == 2:
+            return[["MKD.O5R6.B2", "TCTPH.4R1.B2"],
+                   ["MKD.O5R6.B2", "TCTPH.4R5.B2"]]
+        if self.get_beam() == 1:
+            return [["MKD.O5L6.B1", "TCTPH.4L1.B1"],
+                    ["MKD.O5L6.B1", "TCTPH.4L5.B1"]]
+
+    
+    def get_exciter_name(self, plane):
+        if self.beam() == 1:
+            if self.excitation == AccExcitationMode.ACD:
+                if plane == "H":
+                    return 'MKQA.6L4.B1'
+                elif plane == "V":
+                    return 'MKQA.6L4.B1'
+            elif self.excitation == AccExcitationMode.ADT:
+                if plane == "H":
+                    return "ADTKH.C5L4.B1"
+                elif plane == "V":
+                    return "ADTKV.B5R4.B1"
+        elif self.beam() == 2:
+            if self.excitation == AccExcitationMode.ACD:
+                if plane == "H":
+                    return 'MKQA.6L4.B2'
+                elif plane == "V":
+                    return 'MKQA.6L4.B2'
+            elif self.excitation == AccExcitationMode.ADT:
+                if plane == "H":
+                    return "ADTKH.B5R4.B2"
+                elif plane == "V":
+                    return "ADTKV.C5L4.B2"
+        return None
+    
+    def get_s_first_BPM(self):
+        if self.get_beam() == 1:
+            return self.model_tfs.loc["BPMSW.1L2.B1", "S"]
+        elif self.get_beam() == 2:
+            return self.model_tfs.loc["BPMSW.1L8.B2", "S"]
+        return None
+
+    def get_errordefspath(self):
+        """Returns the path to the uncertainty definitions file (formerly called error definitions file.
+
+        LHC errordefs in the (orthographically incorrect) link "error_deffs.txt"
+        """
+
+        if self.errordefspath is None:
+            return os.path.join(self.modelpath, "error_deffs.txt")
+        else:
+            return self.errordefspath
+
+    def set_errordefspath(self, path):
+        self.errordefspath = path
+
+    def get_k_first_BPM(self, index):
+        if self.get_beam() == 1:
+            model_k =  self.model_tfs.index.get_loc("BPMSW.1L2.B1")
+            while model_k < len(self.model_tfs.index):
+                kname = self.model_tfs.index[model_k]
+                if kname in index:
+                    return index.get_loc(kname)
+                model_k = model_k + 1
+        elif self.get_beam() == 2:
+            model_k =  self.model_tfs.index.get_loc("BPMSW.1L8.B2")
+            while model_k < len(self.model_tfs.index):
+                kname = self.model_tfs.index[model_k]
+                if kname in index:
+                    return index.get_loc(kname)
+                model_k = model_k + 1
+        return None
+        
+    def get_model_tfs(self):
+        return self.model_tfs
+        
+    def get_driven_tfs(self):
+        if self.model_driven is None:
+            return self.model_tfs
+        return self.model_driven
+
+    def get_best_knowledge_model_tfs(self):
+        if self.model_best_knowledge is None:
+            return self.model_tfs
+        return self.model_best_knowledge
+    
+    def get_elements_tfs(self):
+        return self.elements
+
+    def get_elements_centre_tfs(self):
+        return self.elements_centre
+
 
 class _LhcSegmentMixin(object):
 
@@ -427,12 +654,19 @@ class _LhcB1Mixin(object):
     def get_beam(cls):
         return 1
 
+    @classmethod
+    def get_beam_direction(cls):
+        return 1
 
 class _LhcB2Mixin(object):
     @classmethod
     def get_beam(cls):
         return 2
 
+
+    @classmethod
+    def get_beam_direction(cls):
+        return -1
 
 class LhcAts(Lhc):
     MACROS_NAME = "lhc_runII_ats"
