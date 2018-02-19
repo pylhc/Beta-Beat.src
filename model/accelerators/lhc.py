6dfcfee96f0a6ce694eacaa255fea1af1f695765
awegsche <a.wegscheider7141@gmail.com>
2018-02-19 16:37:00 +0100

    checking for more often used names for errordefs

--- model/accelerators/lhc.py
+++ model/accelerators/lhc.py
@@ -292,9 +292,14 @@ class Lhc(Accelerator):
 
         # Error Def #####################################
         self._errordefspath = None
-        errordefspath = os.path.join(self.model_dir, "error_deffs.txt")
+        errordefspath = os.path.join(self.model_dir, "error_deff.txt")
         if os.path.exists(errordefspath):
             self._errordefspath = errordefspath
+        else:  # until we have a proper file name convention
+            errordefspath = os.path.join(self.model_dir, "error_deffs.txt")
+            if os.path.exists(errordefspath):
+                self._errordefspath = errordefspath
+
 
     @classmethod
     def init_and_get_unknowns(cls, args=None):
