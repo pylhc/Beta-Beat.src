Fixed longitudinal location bug, added free2 calculation

optics_measurements/phase.py
@@ -17,8 +17,6 @@ from tfs_files import tfs_pandas
 
 LOGGER = logging_tools.get_logger(__name__)
 PLANES = ("X", "Y")
-# TODO implement free2 calculation of free phase advances:  ph_f2 = ph_d_meas - ph_d_mod + ph_f_mod
-# TODO clean up the special phase advances - separate output file?
 
 
 def calculate_phases(measure_input, input_files, tunes, header_dict):
@@ -51,7 +49,9 @@ def calculate_phases(measure_input, input_files, tunes, header_dict):
         LOGGER.info("Measured tune in plane {} = {}".format(plane, tunes[plane]["Q"]))
         phase_d[plane]["F"], output_dfs = get_phases(measure_input, input_files, model_main, plane)
         headers = _get_headers(header_dict, tunes, plane)
-        write_output(headers, output_dfs, measure_input.outputdir, plane)
+        _write_output(headers, output_dfs, measure_input.outputdir, plane)
+        _write_special_phase_file(plane, phase_d[plane]["F"], tunes[plane]["QF"],
+                                  measure_input.accelerator, measure_input.outputdir, '')
         if measure_input.accelerator.excitation != AccExcitationMode.FREE:
             phase_d[plane]["D"] = phase_d[plane]["F"]
             phase_d[plane]["ac2bpm"] = phase_ac2bpm(phase_d[plane]["F"]["MODEL"], tunes[plane]["Q"],
@@ -59,14 +59,20 @@ def calculate_phases(measure_input, input_files, tunes, header_dict):
             phase_d[plane]["F"], output_dfs = get_phases(measure_input, input_files, model_free,
                         plane, (tunes[plane]["Q"], tunes[plane]["QF"], phase_d[plane]["ac2bpm"]))
             headers = _get_headers(header_dict, tunes, plane, free=True)
-            write_output(headers, output_dfs, measure_input.outputdir, plane)
-        write_special_phase_file(plane, phase_d[plane]["F"], tunes[plane]["QF"], measure_input.accelerator, measure_input.outputdir)
-            # phase_d[plane]["F2"]  = _get_free_phase(phase_d[plane]["F"],
-            # tune_d[plane]["Q"], tune_d[plane]["QF"], bpmsx, model_driven, model, plane)
+            _write_output(headers, output_dfs, measure_input.outputdir, plane)
+            _write_special_phase_file(plane, phase_d[plane]["F"], tunes[plane]["QF"],
+                                      measure_input.accelerator, measure_input.outputdir, '_free')
+            headers = _get_headers(header_dict, tunes, plane, free=True, two=True)
+            phase_d[plane]["F2"] = _get_free_phase(phase_d[plane]["D"], phase_d[plane]["F"])
+            output_dfs = [_create_output_df(phase_d[plane]["F2"], model_free, plane),
+                          _create_output_df(phase_d[plane]["F2"], model_free, plane, tot=True)]
+            _write_output(headers, output_dfs, measure_input.outputdir, plane)
+            _write_special_phase_file(plane, phase_d[plane]["F2"], tunes[plane]["QF"],
+                                      measure_input.accelerator, measure_input.outputdir, '_free2')
     return phase_d
 
 
-def get_phases(meas_input, input_files, model, plane, compensate=None, no_errors=True):
+def get_phases(meas_input, input_files, model, plane, compensate=None, no_errors=False):
     """
     Computes phase advances among all BPMs.
 
@@ -143,48 +149,55 @@ def get_phases(meas_input, input_files, model, plane, compensate=None, no_errors
             phases_3d, period=1, errors=errors_3d, axis=2) % 1.0, phase_frame.index)
     phase_advances["ERRMEAS"] = _get_square_data_frame(stats.circular_error(
             phases_3d, period=1, errors=errors_3d, axis=2), phase_frame.index)
-    return phase_advances, [create_output_df(phase_advances, phase_frame, plane),
-                            create_output_df(phase_advances, phase_frame, plane, tot=True)]
+    return phase_advances, [_create_output_df(phase_advances, phase_frame, plane),
+                            _create_output_df(phase_advances, phase_frame, plane, tot=True)]
 
 
-def write_output(headers, dfs, output, plane):
+def _write_output(headers, dfs, output, plane):
     for head, df in zip(headers, dfs):
         tfs_pandas.write_tfs(join(output, head['FILENAME']), df, head)
         LOGGER.info("Phase advance beating in {} = {}".format(
             head['FILENAME'], stats.weighted_rms(df.loc[:, "DELTAPHASE" + plane])))
 
 
-def create_output_df(phase_advances, model, plane, tot=False):
+def _create_output_df(phase_advances, model, plane, tot=False):
     meas = phase_advances["MEAS"]
     mod = phase_advances["MODEL"]
     err = phase_advances["ERRMEAS"]
-    output_data = model.loc[:, ["S", "MU"+plane]].iloc[:-1, :]
-    output_data.rename(columns={'MU' + plane: 'MU' + plane + 'MDL'}, inplace=True)
     if tot:
-        output_data["NAME"] = model.index[1:].values
+        output_data = model.loc[:, ["S", "MU" + plane]].iloc[:, :]
+        output_data["NAME"] = output_data.index
         output_data = output_data.assign(S2=model.at[model.index[0], "S"], NAME2=model.index[0])
-        output_data["PHASE" + plane] = meas.values[0, 1:]
-        output_data["STDPH" + plane] = err.values[0, 1:]
-        output_data["PH{}MDL".format(plane)] = mod.values[0, 1:]
+        output_data["PHASE" + plane] = meas.values[0, :]
+        output_data["STDPH" + plane] = err.values[0, :]
+        output_data["PH{}MDL".format(plane)] = mod.values[0, :]
     else:
+        output_data = model.loc[:, ["S", "MU" + plane]].iloc[:-1, :]
         output_data["NAME"] = output_data.index
         output_data = output_data.assign(S2=model.loc[:, "S"].values[1:], NAME2=model.index[1:].values)
         output_data["PHASE" + plane] = np.diag(meas.values, k=1)
         output_data["STDPH" + plane] = np.diag(err.values, k=1)
         output_data["PH{}MDL".format(plane)] = np.diag(mod.values, k=1)
+    output_data.rename(columns={'MU' + plane: 'MU' + plane + 'MDL'}, inplace=True)
     dif = (output_data.loc[:, "PHASE" + plane].values -
            output_data.loc[:, "PH{}MDL".format(plane)].values) % 1.0
     output_data["DELTAPHASE" + plane] = np.where(dif > 0.5, dif - 1.0, dif)
     return output_data
 
 
-def _get_headers(header_dict, tunes, plane, free=False):
+def _get_free_phase(driven_phase, free_phase):
+    phase_advances = {"MODEL": free_phase["MODEL"], "ERRMEAS": driven_phase["ERRMEAS"]}
+    phase_advances["MEAS"] = (driven_phase["MEAS"] - driven_phase["MODEL"] + free_phase["MODEL"]) % 1.0
+    return phase_advances
+
+
+def _get_headers(header_dict, tunes, plane, free=False, two=False):
     header = header_dict.copy()
     header['Q1'] = tunes["X"]["QF"] if free else tunes["X"]["Q"]
     header['Q2'] = tunes["Y"]["QF"] if free else tunes["Y"]["Q"]
     header_tot = header.copy()
-    header['FILENAME'] = "getphase" + plane.lower() + free * "_free" + ".out"
-    header_tot['FILENAME'] = "getphasetot" + plane.lower() + free * "_free" + ".out"
+    header['FILENAME'] = "getphase" + plane.lower() + free * "_free" + two * "2" + ".out"
+    header_tot['FILENAME'] = "getphasetot" + plane.lower() + free * "_free" + two * "2" + ".out"
     return [header, header_tot]
 
 
@@ -202,7 +215,7 @@ class PhaseDict(dict):
                          {"ac2bpm": None, "D": None, "F": None, "F2": None})))
 
 
-def write_special_phase_file(plane, phase_advances, plane_tune, accel, outputdir):
+def _write_special_phase_file(plane, phase_advances, plane_tune, accel, outputdir, suffix):
     # TODO REFACTOR AND SIMPLIFY
     plane_mu = "MU" + plane
     meas = phase_advances["MEAS"]
@@ -220,7 +233,7 @@ def write_special_phase_file(plane, phase_advances, plane_tune, accel, outputdir
         if (elements.loc[elem1, "S"] - elements.loc[elem2, "S"]) * bd > 0.0:
             bpm_phase_advance += plane_tune
             model_value += plane_tune
-        bpm_err = err.loc[minmu1, minmu2]
+        bpm_err = phase_advances["ERRMEAS"].loc[minmu1, minmu2]
         phase_to_first = -mus1.loc[minmu1]
         phase_to_second = -mus2.loc[minmu2]
         ph_result = ((bpm_phase_advance + phase_to_first + phase_to_second) * bd)
@@ -238,8 +251,7 @@ def write_special_phase_file(plane, phase_advances, plane_tune, accel, outputdir
                            phase_to_first + phase_to_second,
                            minmu1, minmu2)
         lines.extend([model_desc, result_desc])
-    with open(join(outputdir, 'special_phase' + plane.lower() + '.out'), 'w') as special_phase_writer:
+    with open(join(outputdir, 'special_phase' + plane.lower() + suffix + '.out'), 'w') as special_phase_writer:
         special_phase_writer.write('Special phase advances\n')
         for line in lines:
             special_phase_writer.write(line + '\n')
-